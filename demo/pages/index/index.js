f3f3fa07586a7e2af205bd62b4f4a693913e2f77
132yse <1533540012@qq.com>
2019-06-17 19:17:37 +0800

    commit dayly

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -1,29 +1,34 @@
-import { scheduleWork, options, h } from 'fre'
-function render (vnode) {
-  Page({
-    data: {
-      root: []
-    }
-  })
-  const rootFiber = {
-    tag: 'root',
-    props: { children: vnode }
-  }
-  scheduleWork(rootFiber) // 进入渲染流程
-}
-
-options.commitWork = fiber => {
-  console.log(fiber.children)
-}
-
-function App () {
-  return <text>hello world!</text>
-}
-
-render(<App />)
-
-// Page({
-//   data: {
-//     msg: 'Hello World'
+// import { scheduleWork, options, h } from 'fre'
+// function render (vnode) {
+//   Page({
+//     data: {
+//       root: []
+//     }
+//   })
+//   const rootFiber = {
+//     tag: 'root',
+//     props: { children: vnode }
 //   }
-// })
+//   scheduleWork(rootFiber) // 进入渲染流程
+// }
+
+// options.commitWork = fiber => {
+//   console.log(fiber.children)
+// }
+
+// function App () {
+//   return <text>hello world!</text>
+// }
+
+// render(<App />)
+
+Page({
+  data: {
+    root: {
+      name: "@2",
+      child1: {
+        name: '@1',
+      }
+    }
+  }
+})
