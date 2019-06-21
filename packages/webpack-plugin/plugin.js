0d89d4b3ca1c0138f6ce0e30a253559c29af302c
132yse <1533540012@qq.com>
2019-06-21 09:33:22 +0800

    搞定初版

--- packages/webpack-plugin/plugin.js
+++ packages/webpack-plugin/plugin.js
@@ -9,18 +9,18 @@ class FardWebpackPlugin {
     for (let i = 1; i < this.nodes; i++) {
       blockStr += `
 <template name="@${i}">
-  <block wx:if="{{item.type === 'view'}}">
+  <block wx:if="{{type === 'view'}}">
     <view class="{{props.class}}">
-      <block wx:for="{{props.children}}" wx:key="key">
+      <block wx:for="{{props.children}}" wx:key="">
         <template is="{{item.name}}" data="{{...item}}"></template>
       </block>
     </view>
   </block>
-  <block wx:elif="{{item.type === 'text'}}">{{item.text}}</block>
-  <block wx:elif="{{item.type === 'button'}}">
-    <button class="{{props.class}}" bind:touchstart="{{props.onclick}}">{{props.text}}</button>
+  <block wx:elif="{{type === 'text'}}">{{props.nodeValue}}</block>
+  <block wx:elif="{{type === 'button'}}">
+    <button class="{{props.class}}" bind:touchstart="{{props.onclick}}">{{props.nodeValue}}</button>
   </block>
-  <block wx:elif="{{item.type === 'image'}}">
+  <block wx:elif="{{type === 'image'}}">
     <image class="{{props.class}}" src="{{props.src}}"></image>
   </block>
 </template>` + '\n\r'
