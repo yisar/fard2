bb37aef25e78e6e22420023185213c56fb4238d6
132 <MP_FED@MP-FEDs-iMac.local>
2019-07-05 14:09:34 +0800

    增加webpack用例

---
+++ demo/webpack/pages/index/index.js
@@ -0,0 +1,15 @@
+import { useState } from 'fre'
+import { h, render } from 'fard'
+import './index.styl'
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <view>
+      <text class='text'>{count}</text>
+      <button class='btn' onClick={() => setCount(count + 1)}>+</button>
+    </view>
+  )
+}
+
+render(<Counter />)
