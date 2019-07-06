f5a0392b6388555beed2e2757bb0307f13ac80d9
132yse <1533540012@qq.com>
2019-07-07 00:28:16 +0800

    clean up code

--- demo/webpack/webpack.config.js
+++ demo/webpack/webpack.config.js
@@ -4,57 +4,51 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const FardWebpackPlugin = require('fard-webpack-plugin')
 
 module.exports = {
-    entry: {
-        'pages/index/index': path.resolve(__dirname, 'pages/index/index.js'),
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
-    },
-    optimization: {
-        minimize: false,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
+  entry: {
+    'pages/index/index': path.resolve(__dirname, 'pages/index/index.js')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
 
-            {
-                test: /\.styl$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'stylus-loader',
-                ],
-            },
-        ],
-    },
+      {
+        test: /\.styl$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
+      }
+    ]
+  },
 
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].wxss',
-            chunkFilename: '[id].wxss',
-        }),
+  plugins: [
+    new MiniCssExtractPlugin(),
 
-        new CopyWebpackPlugin([{
-            from: './app.json',
-            to: 'app.json',
-        }]),
+    new CopyWebpackPlugin([
+      {
+        from: './app.json',
+        to: 'app.json'
+      },
+      {
+        from: './app.js',
+        to: 'app.js'
+      }
+    ]),
 
-        new CopyWebpackPlugin([{
-            from: './app.js',
-            to: 'app.js',
-        }]),
-
-        new FardWebpackPlugin({
-            filename: 'bridge.wxml',
-            viewLevel: 20,
-        }),
-    ],
-    mode: 'production'
+    new FardWebpackPlugin({
+      filename: 'bridge.wxml',
+      viewLevel: 20
+    })
+  ],
+  mode: 'production'
 }
